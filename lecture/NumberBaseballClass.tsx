cf6710683bb95f8a165037a6a844da6edaef978f
Gogisung <ggs0707@naver.com>
2020-02-06 11:11:20 +0900

    숫자 야구 Class 적용 publicPath도 수정

--- lecture/NumberBaseballClass.tsx
+++ lecture/NumberBaseballClass.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
-import { useRef, useState, useCallback, Component } from 'react';
 import Try from './Try';
 import { TryInfo } from './types';
 
-const getNumbers = () => {
-  const candidates = [1,2,3,4,5,6,7,8,9];
+const { Component, createRef } = React;
+
+function getNumbers() { // 숫자 네 개를 겹치지 않고 랜덤하게 뽑는 함수
+  const candidate = [1,2,3,4,5,6,7,8,9];
   const array = [];
   for (let i = 0; i < 4; i += 1) {
-    const chosen = candidates.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+    const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
     array.push(chosen);
   }
   return array;
@@ -28,85 +29,92 @@ class NumberBaseball extends Component<{}, State>{
     tries: [],
   }
 
-  onSubmitForm = ((e: React.FormEvent) => {
+  onSubmitForm = (e: React.FormEvent) => {
     const { value, tries, answer } = this.state;
-      e.preventDefault();
-      const input = this.inputRef.current;
-      if (value === answer.join('')) {
-          this.setTries((t) => ([
-              ...t,
-              {
-                  try: value,
-                  result: '홈런!',
-              },
-          ]));
-          setResult('홈런!');
-          alert('게임을 다시 실행합니다.');
-          setValue('');
-          setAnswer(getNumbers());
-          setTries([]);
-          if (input) {
-              input.focus();
-          }
-      } else {
-          const answerArray = value.split('').map((v) => parseInt(v));
-          let strike = 0;
-          let ball = 0;
-          if (tries.length >= 9) {
-              setResult(`10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다!`); // state set은 비동기
-              alert('게임을 다시 시작합니다.');
-              setValue('');
-              setAnswer(getNumbers());
-              setTries([]);
-              if (input) {
-                  input.focus();
-              }
-          } else {
-              console.log('답은', answer.join(''));
-              for (let i = 0; i < 4; i += 1) {
-                  if (answerArray[i] === answer[i]) {
-                      console.log('strike', answerArray[i], answer[i]);
-                      strike += 1;
-                  } else if (answer.includes(answerArray[i])) {
-                      console.log('ball', answerArray[i], answer.indexOf(answerArray[i]));
-                      ball += 1;
-                  }
-              }
-              setTries(t => ([
-                  ...t,
-                  {
-                      try: value,
-                      result: `${strike} 스트라이크, ${ball} 볼입니다.`,
-                  },
-              ]));
-              setValue('');
-              if (input) {
-                  input.focus();
-              }
-          }
+    e.preventDefault();
+    const input = this.inputRef.current;
+    if (value === answer.join('')) {
+      this.setState((prevState) => {
+        return {
+          result: '홈런!',
+          tries: [...prevState.tries, { try: value, result: '홈런!' }],
+        }
+      });
+      alert('게임을 다시 시작합니다!');
+      this.setState({
+        value: '',
+        answer: getNumbers(),
+        tries: [],
+      });
+      if (input) {
+        input.focus();
       }
-  }, [value, answer]);
+    } else { // 답 틀렸으면
+      const answerArray = value.split('').map((v) => parseInt(v));
+      let strike = 0;
+      let ball = 0;
+      if (tries.length >= 9) { // 10번 이상 틀렸을 때
+        this.setState({
+          result: `10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다!`,
+        });
+        alert('게임을 다시 시작합니다!');
+        this.setState({
+          value: '',
+          answer: getNumbers(),
+          tries: [],
+        });
+        if (input) {
+          input.focus();
+        }
+      } else {
+        for (let i = 0; i < 4; i += 1) {
+          if (answerArray[i] === answer[i]) {
+            strike += 1;
+          } else if (answer.includes(answerArray[i])) {
+            ball += 1;
+          }
+        }
+        this.setState((prevState) => {
+          return {
+            tries: [...prevState.tries, { try: value, result: `${strike} 스트라이크, ${ball} 볼입니다`}],
+            value: '',
+          };
+        });
+        if (input) {
+          input.focus();
+        }
+      }
+    }
+  };
 
-  return (
-    <>
-      <h1>{result}</h1>
-      <form onSubmit={onSubmitForm}>
-        <input
-          ref={inputEl}
-          maxLength={4}
-          value={value}
-          onChange={useCallback((e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value), [])}
-        />
-        <button>입력!</button>
-      </form>
-      <div>시도: {tries.length}</div>
-      <ul>
-        {tries.map((v, i) => (
-          <Try key={`${i + 1}차 시도 : ${v.try}`} tryInfo={v} />
-        ))}
-      </ul>
-    </>
-  );
+  onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(this.state.answer);
+    this.setState({
+      value: e.target.value,
+    });
+  };
+
+  inputRef = createRef<HTMLInputElement>(); // this.inputRef
+
+  render() {
+    const { result, value, tries } = this.state;
+    return (
+      <>
+        <h1>{result}</h1>
+        <form onSubmit={this.onSubmitForm}>
+          <input ref={this.inputRef} maxLength={4} value={value} onChange={this.onChangeInput} />
+        </form>
+        <div>시도: {tries.length}</div>
+        <ul>
+          {tries.map((v, i) => {
+            return (
+              <Try key={`${i + 1}차 시도 :`} tryInfo={v} />
+            );
+          })}
+        </ul>
+      </>
+    );
+  }  
 }
 
 export default NumberBaseball;
--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -2,26 +2,25 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    mode: 'development', // production
-    devtool: 'eval', // hidden-source-map
-    resolve: {
-        extensions: ['.jsx', '.js', '.tsx', '.ts'],
-    },
+  mode: 'development', // production
+  devtool: 'eval', // hidden-source-map
+  resolve: {
+    extensions: ['.jsx', '.js', '.tsx', '.ts'],
+  },
 
-    entry: {
-        app: './client'
-    },
-    module: {
-        rules: [{
-            test: /\.tsx?$/,
-            loader: 'awesome-typescript-loader',
-        }]
-    },
-    plugins: [
-        new webpack.LoaderOptionsPlugin({ debug: true }),
-    ],
-    output: {
-        filename: 'app.js',
-        path: path.join(__dirname, 'dist'),
-    }
+  entry: {
+    app: './client'
+  },
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      loader: 'awesome-typescript-loader',
+    }]
+  },
+  plugins: [],
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist',
+  }
 }
