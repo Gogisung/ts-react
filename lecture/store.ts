2780cc8ce13c824a5999494d1e1e52b91ec24f15
Gogisung <ggs0707@naver.com>
2020-02-18 17:59:15 +0900

    UserStore, PostStore TS 만들기

--- lecture/store.ts
+++ lecture/store.ts
@@ -1,24 +1,41 @@
 import { observable, action } from 'mobx';
 
-const userStore = observable({
+export interface User {
+  nickname: string;
+  password: string;
+}
+
+interface UserStore {
+  isLoggingIn: boolean,
+  data: User | null,
+  logIn: (data: User) => void;
+  logOut: () => void;
+}
+
+const userStore = observable<UserStore>({
   isLoggingIn: false,
   data: null,
-  logIn: (data) => {
+  logIn: action((data: User) => {
     userStore.isLoggingIn = true;
     setTimeout(action(() => {
       userStore.data = data;
       userStore.isLoggingIn = false;
       postStore.addPost('hello');
     }), 2000);
-  },
+  }),
   logOut: action(() => {
     userStore.data = null;
   })
 });
 
-const postStore = observable({
+
+interface PostStore {
+  data: string[];
+  addPost: (data: string) => void;
+}
+const postStore = observable<PostStore>({
   data: [],
-  addPost: action((data) => {
+  addPost: action((data: string) => {
     postStore.data.push(data);
   }),
 })
